2706ec82b4a3655f04efbdd40ec05fb1a39531dd
天道总司 <me@zhaoyuxiang.cn>
2021-04-10 11:18:10 +0800

    build: test gh=pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "./routes";
 
 export default createRouter({
-  history: createWebHistory(),
+  // history: createWebHistory(import.meta.env.BASE_URL),  // Disabled due to Github Pages doesn't support this, enable this if you need.
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   scrollBehavior(_to, _from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
