b41b00840f6c3a319e6a5aec5ab06d70b3e7cf79
天道总司 <me@zhaoyuxiang.cn>
2021-04-11 14:12:57 +0800

    fix: 使用新版的路由动画写法

--- src/views/nested/menu1/menu1-2/index.vue
+++ src/views/nested/menu1/menu1-2/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div style="padding: 30px">
     <el-alert :closable="false" title="menu 1-2" type="success">
-      <transition name="fade-transform" mode="out-in">
-        <router-view />
-      </transition>
+      <router-view v-slot="{ Component }">
+        <transition name="fade-transform" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
     </el-alert>
   </div>
 </template>
